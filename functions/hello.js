b99617633a6b33829a8e2531f6021ee71d9bc791
Your Name <you@example.com>
2021-05-20 13:12:12 +0200

    final commit

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,17 @@
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
+const axios = require('axios');
+exports.handler = async function(event, context, callback) {
+    try{
+        const { data } = await axios.get('https://api.genius.com/search?q=' + event.queryStringParameters.param + '&access_token=ZgYwtB7yjBMJz9Kaz2A7By7QBS59Ekeu_30QPjOBHgcZ1TcyDGGsFFzOIPLCw7mA');
+        let response = [];
+        // Grab the items and re-format to the fields we want
+        if (data) {
+            response = data
+        }
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(response),
+        });
+    } catch (err) {
+        callback(err);
+    }
 }
