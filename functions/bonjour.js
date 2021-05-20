27a436f1005dfb338c97df6db482a99dab845560
Your Name <you@example.com>
2021-05-20 13:36:43 +0200

    final commit

---
+++ functions/bonjour.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+exports.handler = async function(event, context, callback) {
+    try{
+        const { data } = await axios.get('https://api.genius.com/songs/' + event.queryStringParameters.param + '?access_token=ZgYwtB7yjBMJz9Kaz2A7By7QBS59Ekeu_30QPjOBHgcZ1TcyDGGsFFzOIPLCw7mA');
+        let response = [];
+        // Grab the items and re-format to the fields we want
+        if (data) {
+            response = data
+        }
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(response),
+        });
+    } catch (err) {
+        callback(err);
+    }
+}
